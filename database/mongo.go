50f22f277536f046235a3107f989d7a728d197fe
Yash-Garg <yash.garg@outlook.in>
2021-09-20 21:46:13 +0530

    chore: use dotenv for DB_URL

--- database/mongo.go
+++ database/mongo.go
@@ -2,17 +2,24 @@ package database
 
 import (
 	"context"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"os"
 )
-
-const connectionString = ""
 
 var MongoInstance = Connection()
 
 func Connection() *mongo.Client {
-	clientOptions := options.Client().ApplyURI(connectionString)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var connectionURL = os.Getenv("DB_URL")
+
+	clientOptions := options.Client().ApplyURI(connectionURL)
 
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.12.3 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
